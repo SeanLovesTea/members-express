4f990e91b1445508c913663cff3ca12d851fad6c
SeanLovesTea <sean.m.concannon@gmail.com>
2023-07-10 17:17:12 +0100

    sign up form and route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs')
+const User = require('./models/user');
+const { body, validationResult } = require('express-validator')
+
 require('dotenv').config()
 
 const mongoDb = process.env.MONGODB_KEY ;
@@ -20,7 +24,48 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user
+  next()
+})
 
 app.get("/", (req, res) => res.render("index"));
+
+app.get("/sign-up",  (req, res) => res.render("sign-up", {errors:[], success:[]}));
+
+const sign_up_post = [
+  body('firstName').trim().isLength({ min:1 }).escape().withMessage('First Name required'),
+  body('lastName').trim().isLength({ min:1 }).escape().withMessage('Last Name required'),
+  body('email').trim().isEmail().isLength({ min:1 }).escape().withMessage('Valid Email required'),
+  
+  async (req, res, next) => {
+    const uniqueEmail = await User.find({ email: req.body.email })
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+      res.render('sign-up', {errors: errors.array(), success:[]})
+    }
+    else if(uniqueEmail.length > 0) {
+      res.render('sign-up', { errors: [{msg: 'email already taken '}], success: []})
+    }else{
+      bcrypt.hash(req.body.password, 10, async function(err, hash) {
+        if (err) return next(err)
+        try {
+          const user = new User({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email,
+            password: hash,
+            member_status: 'Member'
+        })
+        const result = await user.save()
+        res.redirect('/')
+        } catch(err){
+          return next(err)
+        } 
+      })
+  }
+}]
+app.post('/sign-up', sign_up_post)
+
 
 app.listen(8080, () => console.log("app listening on port 8080!"));
--- models/user.js
+++ models/user.js
@@ -1,28 +1,25 @@
 const mongoose = require("mongoose");
-
+const validator = require('validator')
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
-  family_name: { type: String, required: true, maxLength: 100 },
-  email: {
-    email: { type: String,required: true,unique: true,validate: {validator: function(v) {return /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i.test(v)}}}
-  },
-  date_of_birth: { type: Date },
+  last_name: { type: String, required: true, maxLength: 100 },
+  email: { type: String, required: [true, 'Email address is required'] },
   password: { type: String, required: true },
-  status: { type: String, required: true }
+  member_status: { type: String, required: true }
 });
 
 // Virtual for author's full name
-UserSchema.virtual("name").get(function () {
-  return this.family_name + ", " + this.first_name;
-});
+// UserSchema.virtual("name").get(function () {
+//   return this.last_name + ", " + this.first_name;
+// });
 
-// Virtual for author's URL
-UserSchema.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
-  return `/user/${this._id}`;
-});
+// // Virtual for author's URL
+// UserSchema.virtual("url").get(function () {
+//   // We don't use an arrow function as we'll need the this object
+//   return `/user/${this._id}`;
+// });
 
 // Export model
 module.exports = mongoose.model("User", UserSchema);
